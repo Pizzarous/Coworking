84ccf0ead8e39db1459c42cb2b46fa808b350258
Pizzarous <angelocavaqueira@gmail.com>
2023-01-09 17:50:20 +0000

    url fixes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -8,15 +8,15 @@ const Header = () => {
     <Navbar >
       <Container>
         <>
-          <Navbar.Brand href="/">CO@WORK</Navbar.Brand>
-          <Nav.Link href="/aboutus">About Us</Nav.Link>
-          <Nav.Link href="/#">Services</Nav.Link>
-          <Nav.Link href="/#">Calendar</Nav.Link>
-          <Nav.Link href="/#">Events</Nav.Link>
+          <Navbar.Brand href="#/">CO@WORK</Navbar.Brand>
+          <Nav.Link href="#/aboutus">About Us</Nav.Link>
+          <Nav.Link href="#/#">Services</Nav.Link>
+          <Nav.Link href="#/#">Calendar</Nav.Link>
+          <Nav.Link href="#/#">Events</Nav.Link>
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
               <>
-                <Nav.Link href="/contactus">Contact Us &#9990;</Nav.Link>
+                <Nav.Link href="#/contactus">Contact Us &#9990;</Nav.Link>
               </>
             </Nav>
           </Navbar.Collapse>
